6f5c465d30bb7d5014b19cca29b41c4fdf4c35a9
Ismael Hanbel <soportexism4@gmail.com>
2023-11-14 03:56:27 +0100

    first commit

---
+++ src/main/java/com/shieldcommunity/example/Test.java
@@ -0,0 +1,16 @@
+package com.shieldcommunity.example;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class Test extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
